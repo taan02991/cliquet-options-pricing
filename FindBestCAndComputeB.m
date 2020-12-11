7e2cd6302a49757f781c98fc7596f91c1beca933
Thanawat <taan02991@hotmail.com>
2020-12-11 20:44:09 +0700

    FindBestCAndComputeB: add comment

--- FindBestCAndComputeB.m
+++ FindBestCAndComputeB.m
@@ -1,18 +1,30 @@
 function [b, C] = FindBestCAndComputeB(S0, r, sigma0, kappa, theta, delta, rho, T, N, K, Type, M, Cap, Floor, ResetPeriod)
-	X = nan(M, 1);
+
+% This function is used to find the best C variable and compute b which is used in Control Variate technique
+% 
+% For C variable, we compare between payoff of Asian and Euro option  to find which one has 
+% the highest correlation with the payoff of Cliquet option
+% 
+% To compute b:
+% b =  b = sum((X - Xbar) .* (C - Cbar)) / sum((C - Cbar).^2);
+
+    X = nan(M, 1);
 	A = nan(M, 1);
     E = nan(M, 1);
     q = nan(M,1);
     qhat = nan(M,1);
+    % Simulate stock price using heston model and compute the price of Cliquet, Asian and Euro option
 	for i=1:M
 		[S, ~, Shat, ~] = HestonmodelAnti(S0, r, sigma0, kappa, theta, delta, rho, T, N);
   		[X(i, 1),q(i,1),qhat(i,1)] = computeCliquetPrice(S, Shat, N, Type, Cap, Floor, ResetPeriod, r, T);
 		A(i,1) = computeAsianPrice(S,Shat,N,Type,K, r, T);
         E(i, 1) = computeEuroPrice(S,Shat,N,Type,K, r, T);
-end
+    end
+    % Find correlation between (Cliquet, Asian) and (Cliquet, Euro)
     corrA = corrcoef(X,A);
     corrE = corrcoef(X,E);
     Xbar = mean(X);
+    % Choose the option that has the highest correlation to compute the b
     if corrA(1, 2) > corrE(1, 2)
         C = 'A';
         Abar = mean(A);
