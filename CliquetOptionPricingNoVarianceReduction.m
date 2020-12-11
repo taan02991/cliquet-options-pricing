b58c619c8f56f961c683b05d4ac6091054113999
Thanawat <taan02991@hotmail.com>
2020-12-11 21:10:25 +0700

    CliquetOptionPricingNoVarianceReduction: add comment and remove asv

--- CliquetOptionPricingNoVarianceReduction.m
+++ CliquetOptionPricingNoVarianceReduction.m
@@ -1,5 +1,10 @@
 function [P, sigma, CI]=CliquetOptionPricingNoVarianceReduction(S0, r, sigma0, kappa, theta, delta, rho, T, N, K, Type, M, Cap, Floor, ResetPeriod)
-	X = nan(M, 1);
+
+% This function compute Cliquet option price by running simulation M times
+% This version doesn't use any technique to reduce the variance
+
+    X = nan(M, 1);
+    % Simulate stock price and calculate cliquet price
 	for i=1:M
 		[S, ~, ~, ~] = HestonmodelAnti(S0, r, sigma0, kappa, theta, delta, rho, T, N);
   		X(i, 1) = computeCliquetPrice(S, S, N, Type, Cap, Floor, ResetPeriod, r, T);
