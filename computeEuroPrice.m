ea1d930b565a78fd0357b8a38a99619cea27f9dd
Thanawat <taan02991@hotmail.com>
2020-12-11 12:02:14 +0700

    computeEuroPrice: fix bug put option

--- computeEuroPrice.m
+++ computeEuroPrice.m
@@ -4,7 +4,7 @@ function [price] = computeEuroPrice(S, Shat, N, Type, K, r, T)
     if strcmp(Type, 'C')
     		payoff = (max(ST-K, 0) + max(ShatT-K, 0)) / 2;
     	elseif strcmp(Type, 'P')
-    		payoff= (max(K-ST, 0) + max(ShatT-K, 0)) / 2;
+    		payoff= (max(K-ST, 0) + max(K-ShatT, 0)) / 2;
     	else
     		error('"type" must be either ''C'' or ''P''')
     	end
