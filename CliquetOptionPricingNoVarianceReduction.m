f5e5f761dd9c62e51604722d23469d1199c06b10
Thanawat <taan02991@hotmail.com>
2020-12-12 23:20:06 +0700

    refractor: Add CIR and plot edit var plot

--- CliquetOptionPricingNoVarianceReduction.m
+++ CliquetOptionPricingNoVarianceReduction.m
@@ -1,4 +1,4 @@
-function [P, sigma, CI]=CliquetOptionPricingNoVarianceReduction(S0, r, sigma0, kappa, theta, delta, rho, T, N, K, Type, M, Cap, Floor, ResetPeriod)
+function [P, sigma, CI]=CliquetOptionPricingNoVarianceReduction(S0, r0, r_b, rbar, omega, sigma0, kappa, theta, delta, rho, T, N, K, Type, M, Cap, Floor, ResetPeriod)
 
 % This function compute Cliquet option price by running simulation M times
 % This version doesn't use any technique to reduce the variance
@@ -6,6 +6,8 @@ function [P, sigma, CI]=CliquetOptionPricingNoVarianceReduction(S0, r, sigma0, k
     X = nan(M, 1);
     % Simulate stock price and calculate cliquet price
 	for i=1:M
+        % simulate r by CIR model
+        r = CIRModel(r0,r_b,rbar,omega,T,N);
 		[S, ~, ~, ~] = HestonmodelAnti(S0, r, sigma0, kappa, theta, delta, rho, T, N);
   		X(i, 1) = computeCliquetPrice(S, S, N, Type, Cap, Floor, ResetPeriod, r, T);
     end
--- VarNonVarPlot.m
+++ VarNonVarPlot.m
@@ -1,21 +1,25 @@
-function VarNonVarPlot(S0, r, sigma0, kappa, theta, delta, rho, T, N, K, Type, Cap, Floor, ResetPeriod)
+function VarNonVarPlot(S0, r0, r_b, rbar, omega, sigma0, kappa, theta, delta, rho, T, N, K, Type, Cap, Floor, ResetPeriod)
 
 sigmaP1s = [];
 sigmaP2s = [];
+sigmaP3s = [];
 M = [];
 
-for m = 100:100:1000
-    [P1,sigmaP1,CI1] = CliquetOptionPricing(S0, r, sigma0, kappa, theta, delta, rho, T, N, K, Type, m, Cap, Floor, ResetPeriod);
-    [P2,sigmaP2,CI2] = CliquetOptionPricingNoVarianceReduction(S0, r, sigma0, kappa, theta, delta, rho, T, N, K, Type, m, Cap, Floor, ResetPeriod);
+for m = 1000:1000:10000
+    [P1,sigmaP1,CI1] = CliquetOptionPricingControlVariate(S0, r0, r_b, rbar, omega, sigma0, kappa, theta, delta, rho, T, N, K, Type, m, Cap, Floor, ResetPeriod);
+    [P2,sigmaP2,CI2] = CliquetOptionPricingNoVarianceReduction(S0, r0, r_b, rbar, omega, sigma0, kappa, theta, delta, rho, T, N, K, Type, m, Cap, Floor, ResetPeriod);
+    [P3,sigmaP3,CI3] = CliquetOptionPricingAntithetic(S0, r0, r_b, rbar, omega, sigma0, kappa, theta, delta, rho, T, N, K, Type, m, Cap, Floor, ResetPeriod);
     M = cat(2, M, m);
     sigmaP1s = cat(2, sigmaP1s, sigmaP1);
     sigmaP2s = cat(2, sigmaP2s, sigmaP2);
+    sigmaP3s = cat(2, sigmaP3s, sigmaP3);
 end
 figure;
 plot(M, sigmaP1s, '--ob');
 hold on;
 plot(M, sigmaP2s, '--or');
+plot(M, sigmaP3s, '--og');
 hold off;
 xlabel('Number of simulations'); ylabel('sigma');
-legend('Control Variate', 'Non-control variate');
+legend('Control Variate', 'Non-control variate', 'Antithetic');
 end
