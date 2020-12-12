5fdd95a138e37ba36b8aa89d354dd1f3a302d7eb
Thanawat <taan02991@hotmail.com>
2020-12-13 05:05:07 +0700

    fix: syntax error

--- CliquetResetPeriodPlot.m
+++ CliquetResetPeriodPlot.m
@@ -6,7 +6,7 @@ PriceP = [];
 for i = ResetPeriod1:ResetPeriod2
     if mod(N, i) == 0
         ResetPeriod = cat(2, ResetPeriod, i);
-        PriceC = cat(2, PriceC, CliquetOptionPricingAntithetic(S0, r0, r_b, rbar, omega, sigma0,, kappa, theta, delta, rho, T, N, K, 'C', M, Cap, Floor, i));
+        PriceC = cat(2, PriceC, CliquetOptionPricingAntithetic(S0, r0, r_b, rbar, omega, sigma0, kappa, theta, delta, rho, T, N, K, 'C', M, Cap, Floor, i));
         PriceP = cat(2, PriceP, CliquetOptionPricingAntithetic(S0, r0, r_b, rbar, omega, sigma0, kappa, theta, delta, rho, T, N, K, 'P', M, Cap, Floor, i));
     end
 
